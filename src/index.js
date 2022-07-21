7bc662b9fc0e7b59d38a86ee8f8ef2ef6a32fa2d
Carmel <heydarianc@gmail.com>
2022-07-21 17:55:24 -0600

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import Astronome from './Astronome';
 import "./Style.css";
 
-ReactDOM.render( <Astronome />, document.getElementById('root'));
+ReactDOM.render(<HashRouter><Astronome /></HashRouter>, document.getElementById('root'));
 
