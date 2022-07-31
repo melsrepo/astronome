64bf73a150a4256a2b82d2b7fcb7994ea5d7fda2
Carmel <heydarianc@gmail.com>
2022-07-30 21:29:25 -0600

    revert formatting

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,12 +12,8 @@ let navigate = useNavigate();
        <p id = "welcomeMsg">where we connect YOU with the stars.
         <br></br>Please use this application to learn your astrological sign!
         </p>
-   
-      <Row class="justify-content-md-center">
-     <Container >
-     <button id = "homeBtn" onClick = {() => {navigate("/quiz");}} >What's my sign?</button>
-    </Container> 
-    </Row>
+  
+     <Row> <Col md = {3}> </Col> <Col> <button id = "homeBtn" onClick = {() => {navigate("/quiz");}} >What's my sign?</button></Col></Row>
     </div>
   );
 }
